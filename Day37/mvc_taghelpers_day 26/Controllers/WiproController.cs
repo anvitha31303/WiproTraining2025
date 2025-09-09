1908c5a15f99222bc6eb8e82a7dc2ee5df0ef259
anvitha31303 <2000031303@kluniversity.in>
2025-09-09 14:49:39 +0530

    Added my_new_fileday37

---
+++ Day37/mvc_taghelpers_day 26/Controllers/WiproController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_taghelpers_day_26.Controllers
+{
+    public class WiproController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day37/mvc_taghelpers_day 26/Models/CustomHelper1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+
+namespace mvc_taghelpers_day_26.Models
+{
+    public class CustomHelper1
+    {
+        public class MyCustomTagHelper : TagHelper
+        {
+
+            public string Name { get; set; }
+
+            public override void Process(TagHelperContext context, TagHelperOutput output)
+            {
+
+
+                output.TagName = "CustomTagHelper";
+
+
+                output.TagMode = TagMode.StartTagAndEndTag;
+
+                var sb = new StringBuilder();
+
+                sb.AppendFormat("<span>Hi!{0} </ span > ", this.Name);
+
+                output.PreContent.SetHtmlContent(sb.ToString());
+            }
+        }
+    }
+}
